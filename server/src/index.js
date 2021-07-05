318b5f3d69b27c8a9a307589cbfe824dc8f473e6
Pablo Maribondo <pablomgvieira@gmail.com>
2021-07-05 19:56:21 -0300

    feat(server): add mongoose connection

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const routes = require("./routes");
 
 const PORT = process.env.PORT || 3333;
 const app = express();
+
+mongoose.connect(process.env.MONGO_ATLAS_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(express.json());
 app.use(routes);
